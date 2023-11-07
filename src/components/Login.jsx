73eb1ffdfffb89186916a168ac7feb4eceab2dda
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-11-08 02:01:12 +0530

    styles added to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,29 +2,40 @@ import React, { useState } from "react";
 import './login.css';
 const Login = () => {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   function onSubmit(e) {
     e.preventDefault();
+    setEmail('');
+    setPassword('')
   }
   return (
     <>
       <div className="container">
         <div className="login">
+          <h1>Login to Shop</h1>
           <form action="">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              placeholder="Enter your Email ..."
-              onChange={(e)=>setEmail(e.target.value)}
-            />
-            <label htmlFor="password">Email</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter your password "
-            />
-            <input type="submit" onClick={onSubmit} />
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Enter your Email ..."
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+              </div>
+
+              <div className="password">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  placeholder="Enter your password "
+                  onChange={(e)=>setPassword(e.target.value)}
+                  />
+              </div>
+            <input type="submit" className="submit" onClick={onSubmit} />
           </form>
         </div>
       </div>
