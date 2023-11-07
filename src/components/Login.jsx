3c6ff7aedfadbc826625b8b11b32c8bccc2fa8b6
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-11-08 02:47:43 +0530

    login functionality added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,36 +1,57 @@
 import React, { useState } from "react";
 import "./login.css";
 import axios from "axios";
+import { toast } from 'react-hot-toast';
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const onSubmit = async (e) => {
     e.preventDefault();
-    //     const obj = JSON.stringify({
-    //       username: "mor_2314",
-    //       password:  "83r5^_"
-    //   })
-    //     console.log('JSON obj : ',obj)
-    //     const usersName = JSON.stringify({ name: 'John Doe' });
+
+    fetch('https://fakestoreapi.com/users')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+
+
+
     //     const customConfig = {
     //     headers: {
     //     'Content-Type': 'application/json'
     //     }
     // };
-    //     const result = await axios.post('https://fakestoreapi.com/auth/login', obj, customConfig);
-    // const result = await axios.post('https://fakestoreapi.com/auth/login',obj)
-    // console.log('result : ',result)
-    //   fetch('https://fakestoreapi.com/auth/login',{
-    //     method:'POST',
-    //     body:JSON.stringify({
-    //         username: "mor_2314",
-    //         password: "83r5^_"
+    // const result = await axios.post('https://fakestoreapi.com/users',customConfig,{
+    //         body:JSON.stringify(
+    //             {
+    //                 email:'abdul@gmail.com',
+    //                 username:'abdul',
+    //                 password:'12345',
+    //                 name:{
+    //                     firstname:'abdul',
+    //                     lastname:'razzak'
+    //                 },
+    //             }
+    //         )
     //     })
-    // })
-    //     .then(res=>res.json())
-    //     .then(json=>console.log(json))
+          
+    //     console.log('result : ',result);
+
+
+
+        const obj = JSON.stringify({
+          username: "mor_2314",
+          password: "83r5^_"
+      })
+        console.log('JSON obj : ',obj)
+        const customConfig = {
+        headers: {
+        'Content-Type': 'application/json'
+        }
+    };
+    const result = await axios.post('https://fakestoreapi.com/auth/login', obj, customConfig);
+    console.log('result : ',result)
     setEmail("");
     setPassword("");
+    toast.success("Login Successfully !");
   };
   return (
     <>
