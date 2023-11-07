447853357f469fe3c0b8eb5072ca23f9570a1515
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-11-08 01:46:06 +0530

    login page added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import './login.css';
 const Login = () => {
+  const [email, setEmail] = useState("");
+  function onSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="login">
+          <form action="">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Enter your Email ..."
+              onChange={(e)=>setEmail(e.target.value)}
+            />
+            <label htmlFor="password">Email</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password "
+            />
+            <input type="submit" onClick={onSubmit} />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
