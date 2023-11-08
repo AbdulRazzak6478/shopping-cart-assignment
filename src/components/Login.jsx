3a7c4a962239db69dcc1e5a18154a2d9e20ae55e
Abdul Razzak <abdulrazzak74832@gmail.com>
2023-11-08 12:50:05 +0530

    authentication added on login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,88 +1,93 @@
 import React, { useState } from "react";
 import "./login.css";
 import axios from "axios";
-import { toast } from 'react-hot-toast';
-const Login = () => {
-  const [email, setEmail] = useState("");
+import { toast } from "react-hot-toast";
+import {Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
+
+const Login = (props) => {
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [isNav, setIsNav] = useState(false);
+
+  console.log("props : ", props);
+
+  const onSubmitSuccess = (jwtToken) => {
+    // Cookies.set('jwt_token', jwtToken, {
+    //   expires: '1',
+    // });
+    Cookies.set('jwt_token', jwtToken, { expires: 1 });
+    const name = Cookies.get('jwt_token');
+    console.log('token : ',name)
+    setIsNav(true);
+  };
+
   const onSubmit = async (e) => {
     e.preventDefault();
-
-    fetch('https://fakestoreapi.com/users')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-
-
-
-    //     const customConfig = {
-    //     headers: {
-    //     'Content-Type': 'application/json'
+    //     const token = Cookies.get('jwt_token')
+    //     if (token !== undefined) {
+    //       return <Redirect to="/" />
     //     }
-    // };
-    // const result = await axios.post('https://fakestoreapi.com/users',customConfig,{
-    //         body:JSON.stringify(
-    //             {
-    //                 email:'abdul@gmail.com',
-    //                 username:'abdul',
-    //                 password:'12345',
-    //                 name:{
-    //                     firstname:'abdul',
-    //                     lastname:'razzak'
-    //                 },
-    //             }
-    //         )
-    //     })
-          
-    //     console.log('result : ',result);
 
+    // Sai Kiran12:17
+    // import {Redirect} from 'react-router-dom'
+    // import Cookies from 'js-cookie'
 
-
-        const obj = JSON.stringify({
-          username: "mor_2314",
-          password: "83r5^_"
-      })
-        console.log('JSON obj : ',obj)
-        const customConfig = {
-        headers: {
-        'Content-Type': 'application/json'
-        }
+    const obj = JSON.stringify({
+      username: "mor_2314",
+      password: "83r5^_",
+    });
+    console.log("JSON obj : ", obj);
+    const customConfig = {
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
-    const result = await axios.post('https://fakestoreapi.com/auth/login', obj, customConfig);
-    console.log('result : ',result)
-    setEmail("");
+    const result = await axios.post(
+      "https://fakestoreapi.com/auth/login",
+      obj,
+      customConfig
+    );
+    console.log("result : ", result);
+    onSubmitSuccess(result.data.token);
+    setUserName("");
     setPassword("");
     toast.success("Login Successfully !");
   };
   return (
     <>
-      <div className="container">
-        <div className="login">
-          <h1>Login to Shop</h1>
-          <form action="">
-            <div className="email">
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                name="text"
-                value={email}
-                placeholder="Enter your UserName ..."
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
+      {isNav ? (
+        <Navigate to="/" replace={true} />
+      ) : (
+        <div className="container">
+          <div className="login">
+            <h1>Login to Shop</h1>
+            <form action="">
+              <div className="email">
+                <label htmlFor="email">UserName :</label>
+                <input
+                  type="text"
+                  name="text"
+                  value={userName}
+                  placeholder="Enter your UserName ..."
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+              </div>
 
-            <div className="password">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <input type="submit" className="submit" onClick={onSubmit} />
-          </form>
+              <div className="password">
+                <label htmlFor="password">Password :</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <input type="submit" className="submit" onClick={onSubmit} />
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
